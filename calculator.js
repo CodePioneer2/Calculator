2fe5638ad384dbf0ddc3f70967df880dd68ba56b
CodePioneer2 <166389859+CodePioneer2@users.noreply.github.com>
2024-05-30 23:28:12 +0300

    Add basic arithmetic functions

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const add = (a, b) => a + b;
+
+const substract = (a, b) => a - b;
+
+const sum = (array) => {
+    return array.reduce((total, current) => total + current, 0);
+}
+
+const multiply = (array) => {
+    return array.reduce((total, current) => total * current)
+}
+
+const power = (a,b) => Math.pow(a, b);
+
+const factorial = (n) => {
+    if (n === 0) return 1;
+    let product = 1;
+    for (let i = n; i > 0; i--){
+        product *= i;
+    }
+    return product;
+}
